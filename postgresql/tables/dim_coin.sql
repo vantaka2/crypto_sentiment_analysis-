3e521ef341e2fc0d667872ed81d3f1a2c2d07aba
keerthan <keerthanvantakala@gmail.com>
2018-01-19 20:04:03 -0600

    Adding create statement for dim_coin postgres table

---
+++ postgresql/tables/dim_coin.sql
@@ -0,0 +1,12 @@
+--Schema = 'coin'
+CREATE TABLE
+    coin.dim_coin
+    (
+        id TEXT,
+        name TEXT,
+        symbol TEXT,
+        subreddit VARCHAR,
+        website VARCHAR,
+        keywords_to_ignore VARCHAR,
+        keywords_to_include VARCHAR
+    );
